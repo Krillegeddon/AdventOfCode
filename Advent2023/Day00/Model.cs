a318a1c1a319f4b7a1f6211894b7de7c50609170
Christian Jansson <christian.e.jansson@gmail.com>
2023-12-02 18:36:45 +0100

    Added Day00 - template folder...

---
+++ Advent2023/Day00/Model.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent2023.Day00
+{
+    public class Model
+    {
+        public required List<object> Obj { get; set; }
+
+        public static Model Parse()
+        {
+            var retObj = new Model
+            {
+                Obj = new List<object>()
+            };
+
+            foreach (var lx in Data.InputData.Split("\n"))
+            {
+                var l = lx.Trim();
+
+                if (string.IsNullOrEmpty(l))
+                    continue;
+            }
+
+            return retObj;
+        }
+    }
+}
