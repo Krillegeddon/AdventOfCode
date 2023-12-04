9452568200294f6615c72269f32265c998cca5c5
Christian Jansson <christian.jansson@volvo.com>
2023-12-04 10:39:31 +0100

    Done with day 4

--- Advent2023/Day04/Logic.cs
+++ Advent2023/Day04/Logic.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,13 +9,81 @@ namespace Advent2023.Day04
 {
     public class Logic
     {
+        public class CardQueue
+        {
+            public List<Card> Cards = new List<Card>();
+
+            public void Add(Card card, int numCopies)
+            {
+                foreach (var c in Cards)
+                {
+                    if (c == card)
+                    {
+                        card.NumCopies += numCopies;
+                        return;
+                    }
+                }
+                card.NumCopies += numCopies;
+                Cards.Add(card);
+            }
+
+            public Card Dequeue()
+            {
+                if (Cards.Count == 0)
+                    return null;
+
+                var retObj = Cards[0];
+                Cards.RemoveAt(0);
+                return retObj;
+            }
+        }
+
+        private static int CountCorrectNumbers(Card card)
+        {
+            var points = 0;
+            foreach (var givenNum in card.GivenNumbers)
+            {
+                if (card.WinningNumbers.Contains(givenNum))
+                {
+                    points++;
+                }
+            }
+            return points;
+        }
+
         public static string Run()
         {
             var model = Model.Parse();
 
             long sum = 0;
 
-            return sum.ToString();
+            var queue = new CardQueue();
+
+            foreach (var card in model.Cards)
+            {
+                // Add each card to the queue and set it to be one copy of each.
+                queue.Add(card.Value, 1);
+            }
+
+            while (queue.Cards.Count > 0)
+            {
+                var card = queue.Dequeue();
+                var points = CountCorrectNumbers(card);
+
+                for (var i = 0; i < points; i++)
+                {
+                    var newCard = model.Cards[card.Id + 1 + i];
+                    // Add the newCard with as many copies as current card has.
+                    queue.Add(newCard, card.NumCopies);
+                }
+            }
+
+            foreach (var c in model.Cards)
+            {
+                sum += c.Value.NumCopies;
+            }
+
+            return sum.ToString(); //5539496
         }
     }
 }
--- Advent2023/Day04/Model.cs
+++ Advent2023/Day04/Model.cs
@@ -6,15 +6,23 @@ using System.Threading.Tasks;
 
 namespace Advent2023.Day04
 {
+    public class Card
+    {
+        public int Id;
+        public int NumCopies;
+        public List<int> WinningNumbers;
+        public List<int> GivenNumbers;
+    }
+
     public class Model
     {
-        public required List<object> Obj { get; set; }
+        public required Dictionary<int, Card> Cards { get; set; }
 
         public static Model Parse()
         {
             var retObj = new Model
             {
-                Obj = new List<object>()
+                Cards = new Dictionary<int, Card>()
             };
 
             foreach (var lx in Data.InputData.Split("\n"))
@@ -23,6 +31,20 @@ namespace Advent2023.Day04
 
                 if (string.IsNullOrEmpty(l))
                     continue;
+
+                var arr1 = l.Split(':');
+                var arr2 = arr1[1].Split("|");
+                var winArr = arr2[0].Split(" ");
+                var givenArr = arr2[1].Split(" ");
+
+                var card = new Card()
+                {
+                    Id = int.Parse(arr1[0].Replace("Card ", "")),
+                    NumCopies = 0,
+                    WinningNumbers = winArr.Where(p=>!string.IsNullOrEmpty(p)).Select(p => int.Parse(p)).ToList(),
+                    GivenNumbers = givenArr.Where(p => !string.IsNullOrEmpty(p)).Select(p => int.Parse(p)).ToList(),
+                };
+                retObj.Cards.Add(card.Id, card);
             }
 
             return retObj;
