474a0cb1defaba0422a39b747bce79780dd4eb36
Christian Jansson <christian.jansson@volvo.com>
2023-12-07 12:55:05 +0100

    Done with part 1 day 7. Day 2 seems to be non-compliant with my score-calculations from part 1.

--- Advent2023/Day07/Logic.cs
+++ Advent2023/Day07/Logic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Advent2023.Day04;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +9,101 @@ namespace Advent2023.Day07
 {
     public class Logic
     {
+        private static int GetCard(Hand hand, int index)
+        {
+            if (index < 0)
+                return -1;
+            if (index >= hand.Cards.Count)
+                return -1;
+            return hand.Cards[index];
+        }
+
+        public static List<int> GetGroup(Hand hand, int numOfAKind)
+        {
+            var retList = new List<int>();
+            for (int i = 1; i < hand.Cards.Count; i++)
+            {
+                var currentCard = GetCard(hand, i);
+
+                // The card after current must be different in order for it to be a match.
+                if (GetCard(hand, i + 1) == currentCard)
+                    continue;
+
+                // If we are looking for three of a kind, and we are on i=4, then index 4, 3, 2 need to be the same
+                // ... BUT 1 (i-numOfAKind) must be different.
+                if (GetCard(hand, i - numOfAKind) == currentCard)
+                    continue;
+
+                var allSame = true;
+                for (int j = 0; j < numOfAKind; j++)
+                {
+                    // All cards between i and i-numOfAKind should be the same. outside should be different (already checked).
+                    if (GetCard(hand, i - j) != currentCard)
+                        allSame = false;
+                }
+
+                if (allSame)
+                    retList.Add(currentCard);
+            }
+
+            return retList;
+        }
+
+
+
         public static string Run()
         {
             var model = Model.Parse();
 
+            // Calculate score on each hand
+            foreach (var hand in model.Hands)
+            {
+                var kScore = 0;
+                for (var k = 2; k <= 5; k++)
+                {
+                    var kList = GetGroup(hand, k);
+                    if (kList.Count == 1)
+                    {
+                        kScore = k * 2;
+                    }
+                    if (kList.Count == 2)
+                    {
+                        kScore = k * 2 + 1; // Two-pairs...
+                    }
+                    if (k == 3 && kList.Count == 1)
+                    {
+                        var p = GetGroup(hand, 2);
+                        if (p.Count == 1)
+                        {
+                            // Full-house!
+                            kScore = k * 2 + 1;
+                        }
+                    }
+                }
+
+                hand.Score = kScore * 10000000000;
+                hand.Score += hand.CardsUnsorted[0] * 100000000;
+                hand.Score += hand.CardsUnsorted[1] * 1000000;
+                hand.Score += hand.CardsUnsorted[2] * 10000;
+                hand.Score += hand.CardsUnsorted[3] * 100;
+                hand.Score += hand.CardsUnsorted[4] * 1;
+            }
+
+            // Set rank on every hand
+            int r = 1;
+            foreach (var hand in model.Hands.OrderBy(p => p.Score).ToList())
+            {
+                hand.Rank = r;
+                r++;
+            }
+
+            // Calculate sum of all rank * bid.
             long sum = 0;
+            foreach (var hand in model.Hands)
+            {
+                var bid = hand.Rank * hand.BidAmount;
+                sum += bid;
+            }
 
             return sum.ToString();
         }
--- Advent2023/Day07/Model.cs
+++ Advent2023/Day07/Model.cs
@@ -6,23 +6,67 @@ using System.Threading.Tasks;
 
 namespace Advent2023.Day07
 {
+    public class Hand
+    {
+        public List<int> Cards;
+        public List<int> CardsUnsorted;
+        public long BidAmount;
+        public long Score;
+        public int Rank;
+
+    }
+
     public class Model
     {
-        public required List<object> Obj { get; set; }
+        public required List<Hand> Hands { get; set; }
 
         public static Model Parse()
         {
             var retObj = new Model
             {
-                Obj = new List<object>()
+                Hands = new List<Hand>()
             };
 
+            var convDict = new Dictionary<char, int>()
+            {
+                { '2', 2 },
+                { '3', 3 },
+                { '4', 4 },
+                { '5', 5 },
+                { '6', 6 },
+                { '7', 7 },
+                { '8', 8 },
+                { '9', 9 },
+                { 'T', 10 },
+                { 'J', 11 },
+                { 'Q', 12 },
+                { 'K', 13 },
+                { 'A', 14 },
+            };
             foreach (var lx in Data.InputData.Split("\n"))
             {
                 var l = lx.Trim();
 
                 if (string.IsNullOrEmpty(l))
                     continue;
+
+
+                var bidArr = l.Split(" ");
+                var hand = new Hand()
+                {
+                    BidAmount = long.Parse(bidArr[1]),
+                    CardsUnsorted = new List<int>()
+                };
+
+                var cards = bidArr[0].ToCharArray();
+                foreach (var cardChar in cards)
+                {
+                    hand.CardsUnsorted.Add(convDict[cardChar]);
+                }
+
+                int bb = 9;
+                hand.Cards = hand.CardsUnsorted.OrderBy(p => p).ToList();
+                retObj.Hands.Add(hand);
             }
 
             return retObj;
