5415fe4c89219961f22831ce714f5bcda2ca6628
Christian Jansson <christian.e.jansson@gmail.com>
2023-12-13 00:03:58 +0100

    Done with part 1, day 12.

--- Advent2023/Day12/Logic.cs
+++ Advent2023/Day12/Logic.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,32 +9,119 @@ namespace Advent2023.Day12
 {
     public class Logic
     {
-
-        private static long RunRecursive(string row, string groupLenghts)
+        [DebuggerHidden]
+        private static char[] Copy(char[] source)
         {
-            var lenghts = groupLenghts.Split(",");
+            var retObj = new char[source.Length];
+            for (int i = 0; i < source.Length; i++)
+            {
+                retObj[i] = source[i];
+            }
+            return retObj;
+        }
 
+        [DebuggerHidden]
+        private static char[] Copy(char[] source, int skipNum)
+        {
+            var retObj = new char[source.Length - skipNum];
+            for (int i = skipNum; i < source.Length; i++)
+            {
+                retObj[i - skipNum] = source[i];
+            }
+            return retObj;
+        }
+
+
+        private static long RunRecursive(char[] row, string groupLenghts)
+        {
+            if (string.IsNullOrEmpty(groupLenghts))
+            {
+                return 0;
+            }
+            List<int> lenghts = null;
+            try
+            {
+                lenghts = groupLenghts.Split(",").Select(p => int.Parse(p)).ToList();
+            }
+            catch (Exception e)
+            {
+                int bbb = 9;
+            }
+
+            if (row.Length == 0 && groupLenghts.Length > 0)
+                return 0;
 
             // If we have . in the beginning, just skip them and recursively run again.
             var skipWorking = 0;
-            for (var i = 0; row[i] != '.'; i++)
+            for (var i = 0; i<row.Length && row[i] == '.'; i++)
             {
-                skipWorking = i;
+                skipWorking++;
             }
             if (skipWorking > 0)
             {
-                return RunRecursive(row.Substring(skipWorking, row.Length - skipWorking - 1), groupLenghts);
+                return RunRecursive(Copy(row, skipWorking), groupLenghts);
             }
 
-            long sum = 0;
-            for (int i = 0; i < row.Length; i++)
+            // Count number of straight #:s in the beginning... se if they match the first groupLength!
+            var numWrecks = 0;
+            for (var i = 0; i < row.Length && row[i] == '#' ; i++)
             {
-                var delta = RunRecursive(row.Substring(i, row.Length - i), string.Join(',', lenghts.Skip(1)));
-                if (delta > 0)
-                    sum += delta;
+                numWrecks++;
             }
 
-            return sum;
+            if (numWrecks > lenghts.First())
+            {
+                // If we have more wrecks in a row than we've got in first lenghts, then we're bust!
+                return 0;
+            }
+
+            if (numWrecks < lenghts.First())
+            {
+                if (row.Length == numWrecks || row[numWrecks] == '.')
+                {
+                    // If we got fewer wrecks and then an okay spring, then we're bust as well!
+                    return 0;
+                }
+                else
+                {
+                    // If the next char is a ?, then we will check if it checks out if that one is a #. If so,
+                    var nextStr = Copy(row);
+                    var nextLenghts = lenghts.ToList();
+                    nextStr[numWrecks] = '#'; // It must be a # if next run checks out!
+                    var delta = RunRecursive(nextStr, string.Join(',', nextLenghts));
+
+                    var nextStr2 = Copy(row);
+                    var nextLenghts2 = lenghts.ToList();
+                    nextStr2[numWrecks] = '.'; // It must be a # if next run checks out!
+                    var delta2 = RunRecursive(nextStr2, string.Join(',', nextLenghts2));
+
+                    return delta + delta2;
+
+                }
+            }
+
+            if (numWrecks == lenghts.First())
+            {
+                // Hey, we've got a possible match!
+
+                var nextStr = Copy(row, numWrecks);
+                var nextLenghts = lenghts.Skip(1).ToList();
+
+                // If we are done - then there are no nextLenghts, and no more # in the string!
+                if (nextLenghts.Count == 0 && nextStr.Where(p => p == '#').ToList().Count == 0)
+                {
+                    return 1;
+                }
+
+                if (nextStr.Length == 0)
+                    return 0;
+
+                nextStr[0] = '.'; // In case the next recursive run works, then next char is a ., and "next char" is at index 0
+                return RunRecursive(nextStr, string.Join(',', nextLenghts));
+            }
+
+            return 0;
+
         }
 
 
@@ -41,15 +129,15 @@ namespace Advent2023.Day12
         {
             var model = Model.Parse();
 
-            foreach (var sr in model.SpringRows.Skip(2))
+            long sum = 0;
+            foreach (var sr in model.SpringRows)
             {
                 var groups = sr.SpringString.Split('.');
                 var lenghts = sr.GroupLenghts;
-                RunRecursive(sr.SpringString, string.Join(',', lenghts));
+                var num = RunRecursive(sr.SpringString.ToCharArray(), string.Join(',', lenghts));
+                sum += num;
+                int bbb = 9;
             }
-
-
-            long sum = 0;
 
             return sum.ToString();
         }
