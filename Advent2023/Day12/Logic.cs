25e2d59ac8e096e0ec814e52da11c2c599b6059e
Christian Jansson <christian.e.jansson@gmail.com>
2023-12-13 00:41:10 +0100

    Part 2 of Day 12... simple matter of adding a dictionary of already calculated values... a so called "memoization".

--- Advent2023/Day12/Logic.cs
+++ Advent2023/Day12/Logic.cs
@@ -31,11 +31,20 @@ namespace Advent2023.Day12
             return retObj;
         }
 
+        private static Dictionary<string, long> Memo;
 
         private static long RunRecursive(char[] row, string groupLenghts)
         {
+            var memoKey = new string(row) + "_" + groupLenghts;
+
+            if (Memo.ContainsKey(memoKey))
+            {
+                return Memo[memoKey];
+            }
+
             if (string.IsNullOrEmpty(groupLenghts))
             {
+                Memo.Add(memoKey, 0);
                 return 0;
             }
             List<int> lenghts = null;
@@ -49,7 +58,10 @@ namespace Advent2023.Day12
             }
 
             if (row.Length == 0 && groupLenghts.Length > 0)
+            {
+                Memo.Add(memoKey, 0);
                 return 0;
+            }
 
             // If we have . in the beginning, just skip them and recursively run again.
             var skipWorking = 0;
@@ -72,6 +84,7 @@ namespace Advent2023.Day12
             if (numWrecks > lenghts.First())
             {
                 // If we have more wrecks in a row than we've got in first lenghts, then we're bust!
+                Memo.Add(memoKey, 0);
                 return 0;
             }
 
@@ -80,6 +93,7 @@ namespace Advent2023.Day12
                 if (row.Length == numWrecks || row[numWrecks] == '.')
                 {
                     // If we got fewer wrecks and then an okay spring, then we're bust as well!
+                    Memo.Add(memoKey, 0);
                     return 0;
                 }
                 else
@@ -95,8 +109,8 @@ namespace Advent2023.Day12
                     nextStr2[numWrecks] = '.'; // It must be a # if next run checks out!
                     var delta2 = RunRecursive(nextStr2, string.Join(',', nextLenghts2));
 
+                    Memo.Add(memoKey, delta + delta2);
                     return delta + delta2;
-
                 }
             }
 
@@ -110,11 +124,15 @@ namespace Advent2023.Day12
                 // If we are done - then there are no nextLenghts, and no more # in the string!
                 if (nextLenghts.Count == 0 && nextStr.Where(p => p == '#').ToList().Count == 0)
                 {
+                    Memo.Add(memoKey, 1);
                     return 1;
                 }
 
                 if (nextStr.Length == 0)
+                {
+                    Memo.Add(memoKey, 0);
                     return 0;
+                }
 
                 nextStr[0] = '.'; // In case the next recursive run works, then next char is a ., and "next char" is at index 0
                 return RunRecursive(nextStr, string.Join(',', nextLenghts));
@@ -129,7 +147,31 @@ namespace Advent2023.Day12
         {
             var model = Model.Parse();
 
+            // Part 2:
+            if (true)
+            {
+                foreach (var sr in model.SpringRows)
+                {
+                    var s = "";
+                    var cc = "";
+                    for (int i = 0; i < 5; i++)
+                    {
+                        if (s != "")
+                        {
+                            s += "?";
+                            cc += ",";
+                        }
+                        s += sr.SpringString;
+                        cc += string.Join(',', sr.GroupLenghts);
+                    }
+                    sr.SpringString = s;
+                    sr.GroupLenghts = cc.Split(',').Select(p => int.Parse(p)).ToList();
+                }
+            }
+
+            Memo = new Dictionary<string, long>();
             long sum = 0;
+            int r = 0;
             foreach (var sr in model.SpringRows)
             {
                 var groups = sr.SpringString.Split('.');
@@ -137,6 +179,8 @@ namespace Advent2023.Day12
                 var num = RunRecursive(sr.SpringString.ToCharArray(), string.Join(',', lenghts));
                 sum += num;
                 int bbb = 9;
+                r++;
+                Console.WriteLine(r);
             }
 
             return sum.ToString();
